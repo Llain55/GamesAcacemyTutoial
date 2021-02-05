c4cfa611a89aa168ecbb9da11f4d8153f0f1a988
Llain55 <Doherty.n@outlook.com>
2021-02-05 07:56:57 +0000

    Commiting Code

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public int coins = 0;
+    public Text coinsLabel;
+    public GameObject gameOver;
+    public bool hasKey;
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinsLabel.text = "Coins: 0";
+        gameOver.SetActive(false);
+        hasKey = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    public void UpdateCoin()
+    {
+        coins++;
+        coinsLabel.text = "Coins: " + coins;
+    }
+
+    public void GameOver()
+    {
+        gameOver.SetActive(true);
+        Time.timeScale = 0;
+    }
+}
---
+++ Assets/Scripts/TriggerManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerManager : MonoBehaviour
+{
+    public GameController controller;
+
+    public Animator keyGate;
+    public Animator finalGate;
+
+    public AudioClip coinClip;
+    public AudioClip keyClip;
+    public AudioClip winClip;
+
+    public void Start()
+    {
+        finalGate.enabled = false;
+        keyGate.enabled = false;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Coin")
+        {
+            controller.UpdateCoin();
+            this.GetComponent<AudioSource>().PlayOneShot(coinClip,0.4f);
+            other.gameObject.SetActive(false);
+
+        }
+        if (other.tag == "Flag")
+        {
+            controller.GameOver();
+            other.GetComponent<AudioSource>().PlayOneShot(winClip,0.4f);
+        }
+        if (other.tag == "Key")
+        {
+            controller.hasKey = true;
+            this.GetComponent<AudioSource>().PlayOneShot(keyClip,0.4f);
+            other.gameObject.SetActive(false);
+
+        }
+        if (other.tag == "KeyDoorTrigger")
+        {
+            if (controller.coins == 4)
+            {
+                keyGate.enabled = true;
+            }
+        }
+        if (other.tag == "FinalDoorTrigger")
+        {
+            if (controller.hasKey == true)
+            {
+                finalGate.enabled = true;
+            }
+        }
+    }
+}
